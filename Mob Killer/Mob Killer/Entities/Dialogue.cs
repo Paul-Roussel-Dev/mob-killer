08fac263eab754c7535a1b15cf7a338b65e0354f
ROUSSEL <paulroussel144@gmail.com>
2020-01-10 15:19:51 +0100

    Ajout Fonction Dialogue
    oui

--- Mob Killer/Mob Killer/Entities/Dialogue.cs
+++ Mob Killer/Mob Killer/Entities/Dialogue.cs
@@ -1,16 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using Mob_Killer.Data;
 
 namespace Mob_Killer.Entities
 {
     public class Dialogue
     {
-        public int IdDialogue { get; set; }
+        public int Id { get; set; }
 
-        public string TypeDialogue { get; set; }
+        public string Type { get; set; }
 
-        public string TextDialogue { get; set; }
+        public string Text { get; set; }
+
+        public void getDialogue(string type)
+        {
+
+            using (var dboContext = new MobKillerDbContext())
+            {
+                var Dialogues = dboContext.Dialogue.Where(p => p.Type == type);
+                var list = Dialogues.ToList();
+                int nombreEntrees = list.Count;
+                
+                var random = new Random();
+                int choix = random.Next(0, nombreEntrees);
+
+                int compteur = 0;
+
+                foreach (var Dialogue in list)
+                {
+                    if (compteur==choix)
+                    {
+                        Console.WriteLine($"{Dialogue?.Text}");
+                    }
+
+                    compteur++;
+                }
+
+                Console.WriteLine(choix);
+
+            }
+
+        }
 
     }
+
 }
+
+
+
