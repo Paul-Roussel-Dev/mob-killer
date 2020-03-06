c31a434f51ccd44b332ee48d8d503af6923e40e7
ROUSSEL <paulroussel144@gmail.com>
2020-03-06 15:11:57 +0100

    Remplacement des Console.Writeline par des Utils.SlowConsoleWriter

--- Mob Killer/Mob Killer/Entities/Lootphase.cs
+++ Mob Killer/Mob Killer/Entities/Lootphase.cs
@@ -15,25 +15,25 @@ namespace Mob_Killer.Entities
         {
             using var dboContext = new MobKillerDbContext();
             var randomItemA = availableItems[random.Next(0, availableItems.Count)];
-            Console.WriteLine(randomItemA.Name);
+            //Utils.SlowConsoleWriter(randomItemA.Name);
             availableItems = availableItems.Where(i => i != randomItemA).ToList();
             var randomItemB = availableItems[random.Next(0, availableItems.Count)];
-            Console.WriteLine(randomItemB.Name);
+            //Utils.SlowConsoleWriter(randomItemB.Name);
             availableItems = availableItems.Where(i => i != randomItemB).ToList();
 
-            Console.WriteLine(" \n Choisissez à présent l'item de votre choix : \n");
+            Utils.SlowConsoleWriter(" \n Choisissez à présent l'item de votre choix : \n");
 
-            Console.WriteLine("0 = " + randomItemA.Name + "\n" + "1 = " + randomItemB.Name);
+            Utils.SlowConsoleWriter("0 = " + randomItemA.Name + "\n" + "1 = " + randomItemB.Name);
 
             var readeditem = Console.ReadLine();
-            Console.WriteLine("Vous avez choisi : " + readeditem);
+            Utils.SlowConsoleWriter("Vous avez choisi : " + readeditem + "\n");
 
             if (readeditem != "0" && readeditem != "1")
             {
                 do
                 {
-                    Console.WriteLine("Erreur de saisie veuillez écrire quelque chose de valide ! ");
-                    Console.WriteLine("0 = " + randomItemA.Name + "\n" + "1 = " + randomItemB.Name);
+                    Utils.SlowConsoleWriter("Erreur de saisie veuillez écrire quelque chose de valide ! ");
+                    Utils.SlowConsoleWriter("0 = " + randomItemA.Name + "\n" + "1 = " + randomItemB.Name);
                     readeditem = Console.ReadLine();
                 }
                 while (readeditem != "0" || readeditem != "1");
